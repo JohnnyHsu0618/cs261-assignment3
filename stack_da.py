0750550f319883e741a8109907e30f7b98805304
JohnnyHsu0618 <77012437+JohnnyHsu0618@users.noreply.github.com>
2024-05-08 21:52:51 -0700

    stack_da.py

---
+++ stack_da.py
@@ -0,0 +1,121 @@
+# Name:CHE-HAN HSU
+# OSU Email:hsuche@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: assignment 3
+# Due Date:05/06/2024
+# Description: know the concept of Linked List and ADT Implementation
+
+
+from dynamic_array import *
+
+
+class StackException(Exception):
+    """
+    Custom exception to be used by Stack class
+    DO NOT CHANGE THIS METHOD IN ANY WAY
+    """
+    pass
+
+
+class Stack:
+    def __init__(self):
+        """
+        Init new stack based on Dynamic Array
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        self._da = DynamicArray()
+
+    def __str__(self) -> str:
+        """
+        Return content of stack in human-readable form
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        out = "STACK: " + str(self._da.length()) + " elements. ["
+        out += ', '.join([str(self._da[i]) for i in range(self._da.length())])
+        return out + ']'
+
+    def is_empty(self) -> bool:
+        """
+        Return True is the stack is empty, False otherwise
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        return self._da.is_empty()
+
+    def size(self) -> int:
+        """
+        Return number of elements currently in the stack
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        return self._da.length()
+
+    # -----------------------------------------------------------------------
+
+    def push(self, value: object) -> None:
+
+        def push(self, value: object) -> None:    # Add the value to the end of the list
+            self._data.append(value)
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def pop(self) -> object:
+        if not self.stack:
+            raise StackException()
+        return self.stack.pop()
+
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def top(self) -> object:
+
+        if not self.stack:
+            raise StackException("Stack is empty")
+        return self.stack[-1]
+
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+
+# ------------------- BASIC TESTING -----------------------------------------
+
+
+if __name__ == "__main__":
+
+    print("\n# push example 1")
+    s = Stack()
+    print(s)
+    for value in [1, 2, 3, 4, 5]:
+        s.push(value)
+    print(s)
+
+    print("\n# pop example 1")
+    s = Stack()
+    try:
+        print(s.pop())
+    except Exception as e:
+        print("Exception:", type(e))
+    for value in [1, 2, 3, 4, 5]:
+        s.push(value)
+    for i in range(6):
+        try:
+            print(s.pop())
+        except Exception as e:
+            print("Exception:", type(e))
+
+    print("\n# top example 1")
+    s = Stack()
+    try:
+        s.top()
+    except Exception as e:
+        print("No elements in stack", type(e))
+    s.push(10)
+    s.push(20)
+    print(s)
+    print(s.top())
+    print(s.top())
+    print(s)
